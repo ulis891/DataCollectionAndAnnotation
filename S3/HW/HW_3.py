039d9488069132b2d1bf80fddb511adcc1f273fa
Aleksandr Kostromin <ulis89@yandex.ru>
2023-10-22 21:46:25 +0300

    upd HW_3

--- S3/HW/HW_3.py
+++ S3/HW/HW_3.py
@@ -15,17 +15,19 @@ with open("data.json", "r", encoding="UTF-8") as f:
 for book in books:
     info.insert_one(book)
 
-
+#   книги с ценой выше 57
 for doc in info.find({"price": {"$gt": 57}}):
     pprint(doc)
 
+#   книги где в описании есть слово vacation
 for doc in info.find({"description": {"$regex": "[Vv]acation"}}):
     pprint(doc)
 
+#   книги у которых цена от 35 до 40 и в наличии от 5 до 10
 for doc in info.find({"price": {"$gt": 35.0, "$lt": 40.0},
                       "available": {"$gt": 5, "$lt": 10}}):
     pprint(doc)
 
-
+#   книги где в описании есть слово France или цена от от 35 до 40
 for doc in info.find({"$or": [{'description': {"$regex": "France"}}, {"price": {"$gt": 35.0, "$lt": 40.0}}]}):
     pprint(doc)

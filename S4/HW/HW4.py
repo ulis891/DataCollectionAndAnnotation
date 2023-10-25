c7954498ce3f20cb3bdeff6cc1e24858aa4434c9
Aleksandr Kostromin <ulis89@yandex.ru>
2023-10-26 02:02:17 +0300

    upd HW4

--- S4/HW/HW4.py
+++ S4/HW/HW4.py
@@ -16,44 +16,58 @@
 Комментарии для объяснения цели и логики кода.
 """
 
+import csv
+import os
 from pprint import pprint
 import requests
 from fake_useragent import UserAgent
 from lxml import html
 
+
 url = "https://news.mail.ru/"
 ua = UserAgent()
 header = {"UserAgent": ua.chrome}
-session = requests.session()  # что бы маскировать запрос под обычную сессию из браузера
-
+session = requests.session()    # что бы маскировать запрос под обычную сессию из браузера
 response = requests.get(url, headers=header)
 dom = html.fromstring(response.text)
 
-news_links = set(dom.xpath("//body/div[5]/div[2]/div[1]/div[2]/div[1]/div[1]//a/@href"))
+#   добываем все ссылки на новости и чистим их от дубликатов
+news_links = list(dict.fromkeys(dom.xpath("//body/div[5]/div[2]/div[1]/div[2]/div[1]/div[1]//a/@href")))
 
-print(len(news_links))
-
+#   создаём список для всех новостей
 all_news = []
 
+#   проходимся по всем ссылкам
 for item in news_links:
+    #   создаем словарь для каждой новости
+    news_info_dict = dict()
+    #   создаем новый запрос для новостной ссылки
     url = item
     response = requests.get(url, headers=header)
     news = html.fromstring(response.text)
-    # news = dom.xpath("//div[@class='article js-article js-module']")
-    # date = news.xpath("//span/@datetime")
+    #   достаём заголовок
+    news_info_dict['title'] = news.xpath("//h1/text()")[0]
+
+    #   достаём информацию о дате публикации и источнке
     news_info = news.xpath("//span[@class='note']")
-    pprint(len(news_info))
-    data = news_info[0].xpath("//span/@datetime")
-    sorce = news_info[1].xpath("//span[@class='note']/a/span/text()")
+    if news_info:
+        news_info_dict['data'] = news_info[0].xpath("//span/@datetime")[0].split('T')[0]
+        news_info_dict['sorce'] = news_info[1].xpath("//span[@class='note']//span/text()")[2]
+    else:
+        #   если нет источника
+        news_info_dict['data'] = news.xpath("//span//@datetime")[0].split('T')[0]
+        news_info_dict['sorce'] = None
+    #   добавляем ссылку
+    news_info_dict['url'] = url
+    #   добавляем все данные в список
+    all_news.append(news_info_dict)
 
-    print(data, sorce)
-    # for note in news_info:
-    #     date = note.xpath("//span/@datetime")
-    #     sorce = note.xpath("//span[@class='note']/a/span/text()")
-    #     print(date, sorce)
+# pprint(all_news, sort_dicts=False)
 
-    # date = news.xpath("//div[@class='article js-article js-module']//span/@datetime")
-    # source = news.xpath("//div[@class='article js-article js-module']//span/@datetime")
-
-    # pprint(date)
-    break
+#   заролняем информацию по заголовкам csv файла
+fieldnames = ["title", "data", "sorce", "url"]
+#   записываем информацию в файл
+with open(os.getcwd() + "news.csv", "w", encoding="UTF-8", newline="") as csvfile:
+    writer = csv.DictWriter(csvfile, dialect="excel", fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(all_news)
